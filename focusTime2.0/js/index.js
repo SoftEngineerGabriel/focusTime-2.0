d1f5fbe7661374556fd3221b36234d32040e6d10
SoftEngineerGabriel <123767611+SoftEngineerGabriel@users.noreply.github.com>
2023-05-30 22:04:01 -0300

    Add files via upload

--- focusTime2.0/js/index.js
+++ focusTime2.0/js/index.js
@@ -1,50 +1,85 @@
+import Sound from "./sound.js"
+
+const sound = Sound()
+
 let buttonPlay = document.querySelector('.play')
+let buttonStop = document.querySelector('.stop')
 let secondsDisplay = document.querySelector('.seconds')
 let minutesDisplay = document.querySelector('.minutes')
 let buttonAumentar = document.querySelector('.aumentar')
 let buttonDiminuir = document.querySelector('.diminuir')
-let minutes
+let buttonCardFloresta = document.querySelector('.cardFloresta')
+let minutes = Number(minutesDisplay.textContent)
+let timerTimeOut
 
-buttonAumentar.addEventListener('click', function(){
-    let minutes = Number(minutesDisplay.textContent)
-    minutesDisplay.textContent = String(minutes + 1).padStart(2, "0")
-})
 
-buttonDiminuir.addEventListener('click', function(){
-    let minutes = Number(minutesDisplay.textContent)
-    minutesDisplay.textContent = String(minutes -1).padStart(2, "0")
+function updateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  secondsDisplay.textContent = String(seconds).padStart(2, "0")
+}
 
-    if(minutes <= 0){
-        minutesDisplay.textContent = "00"
-    }
-})
+function resetTimer() {
+  updateTimerDisplay(minutes, 0)
+  clearTimeout(timerTimeOut)
+}
+
+
 
 function countdown(){
-    setTimeout(function() {
-      let seconds =  Number(secondsDisplay.textContent)
-      let minutes = Number(minutesDisplay.textContent)
-
-      secondsDisplay.textContent = "00"
-
-      if(minutes <= 0){
+  timerTimeOut = setTimeout(function() {
+    let seconds =  Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
+    
+    secondsDisplay.textContent = "00"
+    
+    if(minutes <= 0){
         return
       }
       
 
       if( seconds <= 0 ) {
-        seconds = 2
-
+        seconds = 60
+        
         minutesDisplay.textContent = String(minutes -1).padStart(2, "0")
       }
       
       secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
       
       countdown()
-
+      
     }, 1000)
-
+    
   }
-
+  
   buttonPlay.addEventListener('click', function(){
     countdown()
-})
+    sound.buttonPressAudio.play()
+  })
+  
+  buttonStop.addEventListener('click', function(){
+    resetTimer()
+    sound.buttonPressAudio.play()
+  })
+
+  buttonAumentar.addEventListener('click', function(){
+      let minutes = Number(minutesDisplay.textContent)
+      minutesDisplay.textContent = String(minutes + 5).padStart(2, "0")
+      sound.buttonPressAudio.play()
+  })
+  
+  buttonDiminuir.addEventListener('click', function(){
+      let minutes = Number(minutesDisplay.textContent)
+      minutesDisplay.textContent = String(minutes -5).padStart(2, "0")
+      sound.buttonPressAudio.play()
+  
+      if(minutes <= 0){
+          minutesDisplay.textContent = "00"
+      }
+  })
+
+
+  buttonCardFloresta.addEventListener('click', function(){
+    sound.buttonPressAudio.play()
+    sound.buttonPressFloresta.play()
+  })
+
---
+++ focusTime2.0/js/sound.js
@@ -0,0 +1,10 @@
+export default function (){
+    const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
+    const buttonPressFloresta = new Audio("https://github.com/madalena-rocha/focus-timer-3.0/blob/bdff2126b7cd1a05168c4d72bf0b11a46c6dd3e6/audio/forest.wav")
+
+
+    return{
+        buttonPressAudio,
+        buttonPressFloresta
+    }
+}
